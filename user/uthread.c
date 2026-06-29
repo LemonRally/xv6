dc249747ace7d8c2b4590ce98b61af6a9d0d2ec3
LemonRally <galmeshy123@gmail.com>
2026-06-29 16:20:45 +0300

    implement uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,31 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context   context;
+  char             stack[STACK_SIZE]; /* the thread's stack */
+  int              state;             /* FREE, RUNNING, RUNNABLE */
+  void             (*func)();
+  int              started; // indicates if thread has been started or not
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +83,13 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    if (current_thread->started == 0)
+    {
+      current_thread->started = 1;
+      current_thread->context.ra = (uint64)current_thread->func;
+      current_thread->context.sp = (uint64)(current_thread->stack + sizeof(current_thread->stack));
+    }
+    thread_switch((uint64)t, (uint64)current_thread);
   } else
     next_thread = 0;
 }
@@ -76,6 +104,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->func = func;
+  t->started = 0;
 }
 
 void 
